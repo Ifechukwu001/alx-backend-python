105beeec8f46345d12c0d89a7d96d7aadcb53147
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-03-30 14:05:48 +0100

    Sum a list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module containing list sum
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Sums a list of float
+    
+    Args:
+        input_list (list of float): Input data.
+        
+    Return:
+        float: sum.
+    """
+    return sum(input_list)
