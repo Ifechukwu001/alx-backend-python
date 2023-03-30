4e4044d4b35035964831a5f0e492b8207afa1bfb
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-03-30 13:52:14 +0100

    Convert float to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module that converts to str
+"""
+
+def to_str(n: float) -> str:
+    """Convert float to string
+    
+    Args:
+        n (float): Float to be converted.
+        
+    Return:
+        str: String representation
+    """
+    return str(n)
