9266ff05402ca00e5051dadcde72f5455ac18e07
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-03-30 13:40:28 +0100

    Concat function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module containing concat function
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings
+    
+    Args:
+        str1 (str): First.
+        str2 (str): Second.
+    
+    Return:
+        str: Concatenated string.
+    """
+    return str1 + str2
