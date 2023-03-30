4cfe37fc0836702b7d4c4a9371e7c42499c741ae
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-03-30 13:47:28 +0100

    Floor function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module containing floor function
+"""
+import math
+
+def floor(n: float) -> int:
+    """Floors a float
+    
+    Args:
+        n (float): Input float
+        
+    Return:
+        int: Output integer
+    """
+    return math.floor(n)
