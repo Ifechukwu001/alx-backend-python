7b1ec363b22098f295eec6204d09df0262a354ae
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-03-30 14:30:28 +0100

    Convert to tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module to create tuple
+"""
+import typing
+
+def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, float]:
+    """Create a tuple
+    
+    Args:
+        k (str): First.
+        v (int | float): Second.
+        
+    Returns:
+        tuple(str, float): Output.
+    """
+    return (k, v * v)
