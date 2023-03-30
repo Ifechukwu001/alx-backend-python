355ce91dd0f32d5e1eee2d4488d08fa8287bd2fe
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-03-30 13:35:55 +0100

    Add function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module containing add function
+"""
+
+def add(a: float, b: float) -> float:
+    """Adds two floats together
+
+    Args:
+        a(float): First.
+        b(float): Second.
+
+    Return:
+        float: Sum.
+    """
+    return a + b
