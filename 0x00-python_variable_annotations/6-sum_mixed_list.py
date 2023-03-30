dd1ecc6c90282e72005a3c8ee2c2c032ea59c288
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-03-30 14:10:25 +0100

    Sum mixed data

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module containing mixed list
+"""
+
+def sum_mixed_list(mxd_list: int | float) -> float:
+    """Sums inputs
+    
+    Args:
+        mxd_list (int | float): Input data.
+        
+    Returns:
+        float: Sum.
+    """
+    return float(sum(mxd_list))
