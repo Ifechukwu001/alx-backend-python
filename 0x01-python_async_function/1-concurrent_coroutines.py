38f4dfa65de72ea858ce4d873c8d6437eddb1675
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-04-08 02:45:15 +0100

    Concurrent loading

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Executes multiple coroutines
+
+"""
+import asyncio
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> float:
+    """Executes async functions
+
+    Args:
+        n (int): Number of times to all async function.
+        max_delay (int): async function.
+
+    Returns:
+        list of floats: List of delay times for the async functions
+    """
+    return await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
