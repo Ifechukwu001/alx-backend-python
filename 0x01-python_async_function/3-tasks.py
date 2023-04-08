6d6de351837f7cbfd629b9e35243ccfe6cbbf039
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-04-08 04:03:37 +0100

    fix annotations

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -3,11 +3,10 @@
 
 """
 import asyncio
-import typing
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-def task_wait_random(max_delay: int) -> typing.Callable:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """Creates an async task
 
     Args:
