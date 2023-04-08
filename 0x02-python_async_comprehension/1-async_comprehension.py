a80367e0a888643e43cfd36abf639fdacaa80a22
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-04-08 05:09:29 +0100

    Async comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Async comprehensions
+
+"""
+import asyncio
+import typing
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> typing.List[float]:
+    """The comprehension function
+
+    Returns:
+        list of floats: The random numbers.
+    """
+    return [num async for num in  async_generator()]
