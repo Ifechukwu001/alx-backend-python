ecaea538214073f334dd48ac54bfc288d93fdd79
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-04-08 04:57:56 +0100

    Async generator function

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Module containing async generator
+
+"""
+import asyncio
+import typing
+import random
+
+
+async def async_generator() -> typing.Iterable[int]:
+    """Generator function
+
+    Yeilds:
+        int: Integer random number.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
+        
